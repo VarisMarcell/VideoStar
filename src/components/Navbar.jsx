ebe0d3c3c14a2d4f5c69199e609ef0616a3054c9
DESKTOP-LQO9OFL\Loaner <meg.ermer33@gmail.com>
2022-11-06 11:29:14 -0500

    rearranges navbar icons -Meg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,9 @@ const Navbar = ({ toggleFavorites, toggleCart }) => {
                 <ul className="navbar-list">
                     <li className="navbar-listItem" onClick={ toggleCart }><i className="bi bi-cart3"></i></li>
                     <li className="navbar-listItem" onClick={ toggleFavorites }><i className="bi bi-heart"></i></li>
-                    {/* <li className="navbar-listItem"><i className="bi bi-sort-alpha-down"></i></li>
-                    <li className="navbar-listItem"><i className="bi bi-stopwatch"></i></li>
-                    <li className="navbar-listItem"><i className="bi bi-currency-dollar"></i></li> */}
+                    <li className="navbar-listItem"><i className="bi bi-sort-down-alt"></i></li>
                 </ul>
-                <div className="navbar-form">
-                    <p className="navbar-listItem"><i className="bi bi-sort-down-alt"></i></p>
+                <div className="navbar-filter">
                     <input type="text" placeholder="Search here" />
                 </div>
 
